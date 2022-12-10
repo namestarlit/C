3bfa71cb6d135fda258b95b2756e3a100d5fffd5
Paul John <rainson.work@gmail.com>
2022-12-10 03:24:05 +0300

    hello world

---
+++ 0x01-The_C_Programming_Language/0x00-starlit/hello.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints hello world
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	printf("Hello, ");
+	putchar('\n');
+	puts("World!");
+
+	return (0);
+}
