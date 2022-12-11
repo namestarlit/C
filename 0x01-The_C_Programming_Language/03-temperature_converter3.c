6354d1716752b78736761bbcee178eae104d1200
Paul John <rainson.work@gmail.com>
2022-12-11 12:31:23 +0300

    Temperature converter using for loop

---
+++ 0x01-The_C_Programming_Language/03-temperature_converter3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints Fahrenheit-Celsius Table
+ *
+ * Description: using a for loop.
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	int fahr;
+
+	for (fahr = 0; fahr <= 300; fahr = fahr + 20)
+		printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
+
+	return (0);
+}
