b43c848f83698c38bbc78d9db43bba5957940fa4
Paul John <rainson.work@gmail.com>
2022-12-11 12:47:23 +0300

    Added heading to the program

--- 0x01-The_C_Programming_Language/03-temperature_converter3.c
+++ 0x01-The_C_Programming_Language/03-temperature_converter3.c
@@ -12,6 +12,8 @@ int main(void)
 {
 	int fahr;
 
+	printf("Fahrenheit-Celsius Table\n");
+
 	for (fahr = 0; fahr <= 300; fahr = fahr + 20)
 		printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
 
