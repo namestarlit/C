52df459821df3851939aaa12406c25df7b119e05
Paul John <rainson.work@gmail.com>
2022-12-28 10:33:21 +0300

    let's C\!

---
+++ 0x02-C_hardway/0x00-starlit/00-hello.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - prints some stuff : )
+ *
+ * Return: 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	puts("Let's get started");
+	puts("C isn't bad to give a try");
+	puts("Between C and I, one of us will C");
+	puts("I will keep on untill");
+	puts("I can do it on my own");
+	putchar('\n');
+	putchar('W');
+	putchar('E');
+	putchar(' ');
+	putchar('W');
+	putchar('I');
+	putchar('L');
+	putchar('L');
+	putchar(' ');
+	putchar('C');
+	putchar('!');
+	putchar('\n');
+
+	return (0);
+}
