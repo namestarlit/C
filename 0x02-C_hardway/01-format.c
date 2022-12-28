5b27d9b72332ce968c7c9763b9f10895abce9229
Paul John <rainson.work@gmail.com>
2022-12-28 12:30:40 +0300

    formated printing

---
+++ 0x02-C_hardway/01-format.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints to console
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int age = 10;
+	int height = 72;
+
+	printf("I am %d years old.\n", age);
+	printf("I am %d inches tall.\n", height);
+
+	return (0);
+}
