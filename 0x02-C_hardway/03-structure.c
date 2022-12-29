0af1088a78ef56f32933e3b41fcd005dadd654db
Paul John <rainson.work@gmail.com>
2022-12-29 07:06:09 +0300

    structure of C program

---
+++ 0x02-C_hardway/03-structure.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - structure of C program
+ *
+ * Return: 0 (success)
+ */
+
+/* This is a comment */
+int main(int argc, char *argv[])
+{
+	int distance = 100;
+
+	//this is also a comment
+	printf("You are %d miles away.\n", distance);
+
+	return (0);
+}
