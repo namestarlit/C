c23e608eb9c86b9a03b5097a211ddf2791924816
Paul John <rainson.work@gmail.com>
2023-01-08 20:36:27 +0300

    a little infinite while loop

---
+++ 0x02-C_hardway/0x00-starlit/09-curious.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+/**
+ * main - trying to see how the while loop
+ * will react from this
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	while (true)
+		puts("I will print this line forever");
+
+	return (0);
+}
