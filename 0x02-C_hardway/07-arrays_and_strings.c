720615f09fb645c34413d5fe0593dc94abb53d93
Paul John <rainson.work@gmail.com>
2023-12-17 10:21:28 +0300

    update

--- 0x02-C_hardway/07-arrays_and_strings.c
+++ 0x02-C_hardway/07-arrays_and_strings.c
@@ -14,9 +14,7 @@ int main(int __attribute__((unused)) argc, char __attribute__((unused)) *argv[])
 
 	/* first, print them out raw */
 	printf("numbers: %d %d %d %d\n", numbers[0], numbers[1], numbers[2], numbers[3]);
-
 	printf("name each: %c %c %c %c\n", name[0], name[1], name[2], name[3]);
-
 	printf("name: %s\n", name);
 
 	/* set up numbers */
